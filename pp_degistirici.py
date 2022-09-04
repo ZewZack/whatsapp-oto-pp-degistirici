9e1bbadca22e259e2da108a6f10e7edb26a82946
Enes a.k.a ZewZack <54986847+ZewZack@users.noreply.github.com>
2022-09-04 16:54:45 +0300

    pp_degistirici v4

--- pp_degistirici.py
+++ pp_degistirici.py
@@ -2,23 +2,67 @@
 #   whatsapp otomatik profil fotoğrafı değiştirici
 #   by @zewzack
 #
+from doctest import master
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import random, os
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from importlib.resources import path
+from tkinter import *
 
 options = Options()
 options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 chrome_executable = Service(executable_path='chromedriver.exe', log_path='NUL')
 driver = webdriver.Chrome(service=chrome_executable)
 driver.get('https://web.whatsapp.com/')
+sleep(1)
 
-sleep(3)
-input('qr kodu okuttuktan sonra herhangi bir tuşa basınız...')
- 
+# QR kodu okuttuktan sonra devam etmek için butona basın.
+def devamet():
+    window.destroy()
+
+window = tk.Tk()
+window.eval('tk::PlaceWindow . center')
+button = tk.Button(
+    text="QR kodu okuttuktan sonra bana tıkla",
+    width=50,
+    height=10,
+    bg="black",
+    fg="white",
+    command=devamet
+)
+button.pack()
+window.mainloop()
+# butona basınca üstteki devamet() fonksiyonu window.destroy() eder ve program devam eder.
+
+# ___________________________________________________________________________________________________________________________________
+
+win = Tk()
+win.geometry("750x250")
+
+def kapat():
+   win.destroy()
+
+Label(win, text="resimlerin olduğu dosyayı seçin:", font=('Aerial 18 bold')).pack(pady=20)
+button= ttk.Button(win, text="Select", command= kapat)
+
+button.pack(ipadx=5, pady=15)
+win.mainloop()
+
+dir = filedialog.askdirectory(title="dosya dizini seçin")
+
+# dir konumu kullanıcıdan alındı.
+
+# __________________________________________________________________________________________________________________________________
+
 for x in range(100):
- dir = ("C:\\Users\\zewza\\Pictures\\Saved Pictures\\")
+ #dir = input("Resimlerin alınacağı konumu giriniz: ") # C:/Users/zewza/Pictures/Saved Pictures/
+ #dir = ("C:/Users/zewza/Pictures/Saved Pictures/")
  random_picture = random.choice(os.listdir(dir))
  random_filename = (dir,random_picture)
  
